e510b583afff2ba93750fd917647349f4377dd3b
nitin1438 <nitin.chobhe@gmail.com>
2017-01-26 23:31:56 +0530

    Liquidify shamu

--- liquid.mk
+++ liquid.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
@@ -11,11 +11,15 @@ $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_shamu
+PRODUCT_NAME := liquid_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 
 TARGET_VENDOR := moto
+
+#Ship Camera 2
+PRODUCT_PACKAGES += \
+    Camera2
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
