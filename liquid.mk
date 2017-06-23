0c2cf7e3e58808438d6dbeb52feff6a661949986
nitin1438 <nitin.chobhe@gmail.com>
2017-06-23 22:20:12 +0530

    Update shamu trees

--- liquid.mk
+++ liquid.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 2560
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
@@ -11,13 +11,17 @@ $(call inherit-product, device/moto/shamu/aosp_shamu.mk)
 DEVICE_PACKAGE_OVERLAYS += device/moto/shamu/overlay-cm
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_shamu
+PRODUCT_NAME := liquid_shamu
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 6
 
 TARGET_VENDOR := moto
 
+#Ship Camera 2
+PRODUCT_PACKAGES += \
+    Camera2
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=shamu \
-    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F27E/3939304:user/release-keys \
-    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F27E 3939304 release-keys"
+    BUILD_FINGERPRINT=google/shamu/shamu:7.1.1/N6F26Q/3549652:user/release-keys \
+    PRIVATE_BUILD_DESC="shamu-user 7.1.1 N6F26Q 3549652 release-keys"
